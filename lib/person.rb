510f7bd5a7c85627ae05986a6f0e226b46bc3675
Steven Hunal <stevehunal@gmail.com>
2019-08-10 02:17:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,6 @@
 class Person
-  attr_accessor :name, :user_name, :age, :location, :bio
  
   def initialize(hash)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    hash.each {|key, value| self.send(("#{key}="), value)}
   end
 end
